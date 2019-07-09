1cfc6d2f7a187aed586d3dfb5d7f39db6b2f1b7a
alexandergr <alexanderg@hexagontech.co.il>
2019-07-09 17:32:17 +0300

    init project

---
+++ Garage/Car.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+namespace Garage 
+{
+    class Car
+    {   
+        public Car(string name)
+        {
+            this.consumptionsStore = new List<double>(); 
+            this.name = name;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public void AddConsumption(List<double> tripConsumptions)
+        {   
+            foreach (double consumption in tripConsumptions) 
+            {
+                this.consumptionsStore.Add(consumption);
+            }
+        }
+        public double getAverageConsumption()
+        {
+            var result = 0.0;
+            if (this.consumptionsStore.Count > 0)
+            {
+                foreach (double consumption in this.consumptionsStore)
+                {
+                    result += consumption;
+                }
+                return result/this.consumptionsStore.Count;
+            } else {
+                return 0.0;
+            }
+        }
+
+        public void showStatictic()
+        {
+            var highConsumption = double.MinValue;
+            var lowConsumption = double.MaxValue;
+
+            foreach (double consumption in this.consumptionsStore)
+            {
+                highConsumption = Math.Max(consumption, highConsumption);
+                lowConsumption = Math.Min(consumption, lowConsumption);
+            }
+            Console.WriteLine($"Max consumption in trip {highConsumption} l/100");
+            Console.WriteLine($"Min consumption in trip {lowConsumption} l/100");
+        }
+
+        private string name;
+        private List<double> consumptionsStore;
+
+    }
+}
---
+++ Garage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var honda = new Car("Honda civic");
+            var tripConsumptions  = new List<double> () {9.5, 11.5, 12.1, 6.8, 13.2};
+            honda.AddConsumption(tripConsumptions);
+
+            Console.WriteLine($"Avarage consumption of {honda.getName()} is {honda.getAverageConsumption()} ");
+            honda.showStatictic();
+        }
+    }
+}
